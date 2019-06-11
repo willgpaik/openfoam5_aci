9d38f0304c658b2a603e897270ce98ec77d85e37
willgpaik <44753879+willgpaik@users.noreply.github.com>
2019-06-11 18:07:53 -0400

    Update cfdem_install.sh

--- cfdem_install.sh
+++ cfdem_install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+export LD_LIBRARY_PATH=/opt/openfoam5/platforms/linux64GccDPInt32Opt/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH
+export CPATH=/usr/include:$CPATH
+export CPATH=/usr/lib/openmpi/include:$CPATH
+export MPI_ROOT=/usr/lib/openmpi
+export MPI_ARCH_FLAGS="-DMPICH_SKIP_MPICXX"
+export MPI_ARCH_INC="-I$MPI_ARCH_PATH/include"
+export MPI_ARCH_LIBS='-L$(MPI_ARCH_PATH)/lib -lmpich -lmpichcxx -lmpl -lopa -lrt'
 . /opt/openfoam5/etc/bashrc
-
 cd /opt/sw
 BASE=$PWD
 
@@ -14,6 +23,13 @@ git clone git://github.com/CFDEMproject/LIGGGHTS-PUBLIC.git
 git clone git://github.com/CFDEMproject/LPP.git lpp
 
 cd $BASE
+
+export PATH=$PATH:/opt/sw/LIGGGHTS/lpp/src:/opt/sw/LIGGGHTS-PUBLIC/src
+export PATH=$PATH:/opt/sw/LIGGGHTS/LIGGGHTS-PUBLIC/lib/vtk/install/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/openfoam5/platforms/linux64Gcc48DPInt32Opt/lib
+export LD_LIBRARY_PATH:$LD_LIBRARY_PATH:/opt/sw/LIGGGHTS/LIGGGHTS-PUBLIC/lib/vtk/install/lib
+export CPATH=$CPATH:/opt/openfoam5/platforms/linux64Gcc48DPInt32Opt/include
+export CPATH=$CPATH:/opt/sw/LIGGGHTS/IGGGHTS-PUBLIC/lib/vtk/install/include/vtk-8.0
 
 export CFDEM_VERSION=PUBLIC
 export CFDEM_PROJECT_DIR=/opt/sw/CFDEM/CFDEMcoupling-$CFDEM_VERSION-$WM_PROJECT_VERSION
