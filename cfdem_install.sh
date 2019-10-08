370f22b3c1c7c8596d4ba7f90a918682bfadde8b
willgpaik <44753879+willgpaik@users.noreply.github.com>
2019-10-08 16:47:51 -0400

    Update cfdem_install.sh

--- cfdem_install.sh
+++ cfdem_install.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+# installation script updated for Centos 7 ver.
 
 cd /opt/sw
 BASE=$PWD
 
-export LD_LIBRARY_PATH=/opt/openfoam5/platforms/linux64GccDPInt32Opt/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/sw/OpenFOAM/OpenFOAM-5.x/platforms/linux64GccDPInt32Opt/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH
 export CPATH=/usr/include:$CPATH
 export CPATH=/usr/lib/openmpi/include:$CPATH
-export MPI_ROOT=/usr/lib/openmpi
+export MPI_ROOT=/opt/sw/OpenFOAM/ThirdParty-5.x/platforms/linux64Gcc/openmpi-2.1.1
 export MPI_ARCH_FLAGS="-DMPICH_SKIP_MPICXX"
 export MPI_ARCH_INC="-I$MPI_ARCH_PATH/include"
 export MPI_ARCH_LIBS='-L$(MPI_ARCH_PATH)/lib -lmpich -lmpichcxx -lmpl -lopa -lrt'
-. /opt/openfoam5/etc/bashrc
+. /opt/sw/OpenFOAM/OpenFOAM-5.x/etc/bashrc
 
 mkdir CFDEM
 mkdir LIGGGHTS
@@ -31,9 +32,9 @@ cd $BASE
 
 export PATH=$PATH:/opt/sw/LIGGGHTS/lpp/src:/opt/sw/LIGGGHTS-PUBLIC/src
 export PATH=$PATH:/opt/sw/LIGGGHTS/LIGGGHTS-PUBLIC/lib/vtk/install/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/openfoam5/platforms/linux64Gcc48DPInt32Opt/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/sw/OpenFOAM/OpenFOAM-5.x/platforms/linux64Gcc48DPInt32Opt/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/sw/LIGGGHTS/LIGGGHTS-PUBLIC/lib/vtk/install/lib
-export CPATH=$CPATH:/opt/openfoam5/platforms/linux64Gcc48DPInt32Opt/include
+export CPATH=$CPATH:/opt/sw/OpenFOAM/OpenFOAM-5.x/platforms/linux64Gcc48DPInt32Opt/include
 export CPATH=$CPATH:/opt/sw/LIGGGHTS/IGGGHTS-PUBLIC/lib/vtk/install/include/vtk-8.0
 
 export CFDEM_VERSION=PUBLIC
