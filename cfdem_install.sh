98287f9a516859f148cc6023433753483b657ba9
willgpaik <44753879+willgpaik@users.noreply.github.com>
2019-10-12 12:59:05 -0400

    Update cfdem_install.sh

--- cfdem_install.sh
+++ cfdem_install.sh
@@ -26,9 +26,12 @@ cd $BASE/LIGGGHTS
 git clone git://github.com/CFDEMproject/LIGGGHTS-PUBLIC.git
 git clone git://github.com/CFDEMproject/LPP.git lpp
 cd LIGGGHTS-PUBLIC/src/MAKE
-sed -i -e '22s/.*/AUTOINSTALL_VTK = "ON"/g' Makefile.user_default
-sed -i -e '130s#.*#VTK_INC_USR=-I$BASE/LIGGGHTS/LIGGGHTS-PUBLIC/lib/vtk/install/include/vtk-8.0#g' Makefile.user_default
-sed -i -e '132s#.*#VTK_LIB_USR=-L$BASE/LIGGGHTS/LIGGGHTS-PUBLIC/lib/vtk/install/lib#g' Makefile.user_default
+#sed -i -e '22s/.*/AUTOINSTALL_VTK = "ON"/g' Makefile.user_default
+#sed -i -e '130s#.*#VTK_INC_USR=-I$BASE/LIGGGHTS/LIGGGHTS-PUBLIC/lib/vtk/install/include/vtk-8.0#g' Makefile.user_default
+#sed -i -e '132s#.*#VTK_LIB_USR=-L$BASE/LIGGGHTS/LIGGGHTS-PUBLIC/lib/vtk/install/lib#g' Makefile.user_default
+sed -i -e '12s/.*/AUTOINSTALL_VTK = "ON"/g' Makefile.user
+sed -i -e '120s#.*#VTK_INC_USR=-I$BASE/LIGGGHTS/LIGGGHTS-PUBLIC/lib/vtk/install/include/vtk-8.0#g' Makefile.user
+sed -i -e '122s#.*#VTK_LIB_USR=-L$BASE/LIGGGHTS/LIGGGHTS-PUBLIC/lib/vtk/install/lib#g' Makefile.user
 
 cd $BASE
 
